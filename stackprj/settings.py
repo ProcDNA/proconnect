7fd7140faf4d61efba935185b15898050a7da33a
gaur-procdna <himanshu.gaur@procdna.com>
2024-09-27 19:18:38 +0530

    debug true

--- stackprj/settings.py
+++ stackprj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jaelsf1%p)ryf^8w7-&o9e+4rqpiyn9*j_!3r1q#t@&+b!8+bh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*", "https://proconnect-zriz.onrender.com"]
 
